64da93e73de3942654f8b1f7a50aa4e6cd73b1ff
Christian Dreier <flipper.x3r@googlemail.com>
2017-10-03 19:22:23 +0200

    Add script for creating a new presentation

---
+++ create.ps1
@@ -0,0 +1,41 @@
+Param(
+    [string]$path
+)
+
+$files = @(
+    "out",
+    "boilerplate-local.html"
+)
+
+function Error([string]$msg) {
+    Write-Error $msg
+    exit 1
+}
+
+function PerformCopy([string]$file, [string]$dst) {
+    $srcPath = Join-Path -Path $PSScriptRoot -ChildPath $file
+    $dstPath = Join-Path -Path $dst -ChildPath $file
+    if (-Not (Test-Path $dstPath)) {
+        Copy-Item $srcPath $dstPath -Recurse >$null 2>&1
+    } else {
+        Error "Already existing: $dstPath"
+    }
+}
+
+if ($MyInvocation.BoundParameters.Keys -match 'path') {
+    $destPath = $path
+} else {
+    $destPath = Read-Host -Prompt "Give destination path"
+}
+
+if (Test-Path $destPath) {
+    if (-Not (Test-Path $destPath -PathType Container)) {
+        Error "Given path does not lead to a directory"
+    }
+} else {
+    New-Item -ItemType directory -Path $destPath >$null 2>&1
+}
+
+foreach ($file in $files) {
+    PerformCopy $file $destPath
+}
